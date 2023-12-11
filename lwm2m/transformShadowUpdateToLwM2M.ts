51cb1b32828b99b1d201a9c1e374427e75ece0a7
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-12-11 12:41:30 +0100

    fix(lwm2m): improve message handling

--- lwm2m/transformShadowUpdateToLwM2M.ts
+++ lwm2m/transformShadowUpdateToLwM2M.ts
@@ -19,20 +19,71 @@ export const transformShadowUpdateToLwM2M = (
 	transformers: Readonly<Array<Transformer>>,
 ): ((update: Update) => Promise<ReturnType<typeof senMLtoLwM2M>>) => {
 	// Turn the JSONata in the transformers into executable functions
-	const transformerFns = transformers.map(({ match, transform }) => ({
-		match: jsonata(match),
-		transform: jsonata(transform),
-	}))
+	const transformerFns: Array<{
+		match: ReturnType<typeof jsonata>
+		matchExpression: string
+		transform: ReturnType<typeof jsonata>
+		transformExpression: string
+	}> = []
+
+	for (const {
+		match: matchExpression,
+		transform: transformExpression,
+	} of transformers) {
+		let match: ReturnType<typeof jsonata>
+		let transform: ReturnType<typeof jsonata>
+		try {
+			match = jsonata(matchExpression)
+		} catch {
+			throw new Error(`Failed to parse match expression '${matchExpression}'`)
+		}
+		try {
+			transform = jsonata(transformExpression)
+		} catch {
+			throw new Error(
+				`Failed to parse match expression '${transformExpression}'`,
+			)
+		}
+		transformerFns.push({
+			match,
+			matchExpression,
+			transform,
+			transformExpression,
+		})
+	}
 
 	return async (input: Update): Promise<Array<LwM2MObjectInstance>> =>
 		Promise.all(
-			transformerFns.map(async ({ match, transform }) => {
-				// Check if the `matched` JSONata returns `true`.
-				const matched = await match.evaluate(input)
-				if (typeof matched !== 'boolean' || matched !== true) return null
-				// Apply the transform
-				return transform.evaluate(input)
-			}),
+			transformerFns.map(
+				async ({ match, matchExpression, transform, transformExpression }) => {
+					// Check if the `matched` JSONata returns `true`.
+					try {
+						const matched = await match.evaluate(input)
+						if (typeof matched !== 'boolean') return null
+						if (matched === false) return null
+					} catch (err) {
+						console.error(err)
+						console.error(
+							`Failed to match ${JSON.stringify(
+								input,
+							)} using expression '${matchExpression}'!`,
+						)
+						return false
+					}
+					// Transform
+					try {
+						return await transform.evaluate(input)
+					} catch (err) {
+						console.error(err)
+						console.error(
+							`Failed to transform ${JSON.stringify(
+								input,
+							)} using expression '${transformExpression}'!`,
+						)
+						return null
+					}
+				},
+			),
 		)
 			.then((result) => result.flat())
 			// Ignore unmatched transformers
