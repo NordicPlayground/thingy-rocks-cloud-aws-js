be7e230854786039f534b1abfc51dd2c5945e5ff
Lena Kråkevik Haraldseid <35926103+Lenakh97@users.noreply.github.com>
2025-10-09 09:36:10 +0200

    Update nordicNRPlus/validateInput.spec.ts
    Co-authored-by: Markus Tacker <Markus.Tacker@NordicSemi.no>

--- nordicNRPlus/validateInput.spec.ts
+++ nordicNRPlus/validateInput.spec.ts
@@ -3,7 +3,7 @@ import assert from 'node:assert'
 import { describe, it } from 'node:test'
 import { inputSchemaLwm2mMessage } from '../lambda/webhookHandler.ts'
 
-void describe('validateExampleInput', () => {
+void describe('inputSchemaLwm2mMessage', () => {
 	const jsonString = `{"type":"device.messages","messages":[{"teamId":"123123","deviceId":"123123","messageId":"b6903898-9998-4767-a7c2-123123123","topic":"$aws/things/123123/shadow/update/documents","message":{"previous":null,"current":{"state":{"desired":{"nrfcloud_mqtt_topic_prefix":"prod/randomTeamId/","pairing":{"state":"paired","topics":{"d2c":"prod/randomNum/m/d/randomNum/d2c","c2d":"prod/123/m/d/123123/+/r"}}}},"metadata":{"desired":{"nrfcloud_mqtt_topic_prefix":{"timestamp":1759240425},"pairing":{"state":{"timestamp":1759240425},"topics":{"d2c":{"timestamp":1759240425},"c2d":{"timestamp":1759240425}}}}},"version":1},"timestamp":1759240425},"receivedAt":"2025-09-30T13:53:45.262Z"}],"timestamp":"2025-10-01T12:23:52.583464498Z"}`
 	const parsed = JSON.parse(jsonString)
 
