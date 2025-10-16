ef0d7a5bce6623f7fd11b7a8b4891fbb668179e4
Lena Kråkevik Haraldseid <35926103+Lenakh97@users.noreply.github.com>
2025-10-16 11:57:58 +0200

    Update lambda/ensureThingExists.ts
    Co-authored-by: Markus Tacker <Markus.Tacker@NordicSemi.no>

--- lambda/ensureThingExists.ts
+++ lambda/ensureThingExists.ts
@@ -1,15 +1,20 @@
 import { CreateThingCommand, type IoTClient } from '@aws-sdk/client-iot'
 import { thingExists } from '../nordicNRPlus/thingExists.ts'
 
+
 export const ensureThingExists =
-	(iotClient: IoTClient) =>
-	async (thingName: string): Promise<void> => {
-		if ((await thingExists(iotClient, thingName)) === false) {
-			await iotClient.send(
-				new CreateThingCommand({
-					thingName,
-					thingTypeName: 'nordic-nrplus',
-				}),
-			)
-		}
+	(iotClient: IoTClient, thingExists: EnsureThingFn) => {
+	
+	const e = thingExists(iotClient)
+	
+	return async (thingName: string): Promise<void> => {
+	    if (await e(thingName)) return
+		await iotClient.send(
+			new CreateThingCommand({
+				thingName,
+				thingTypeName: 'nordic-nrplus',
+			}),
+		)
+	}
+	
 	}
