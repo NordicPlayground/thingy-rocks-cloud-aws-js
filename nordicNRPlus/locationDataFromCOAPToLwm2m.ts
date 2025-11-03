c86db60e564c514b09ebb0e7d51ffb5e2a30d2b7
Markus Tacker <Markus.Tacker@NordicSemi.no>
2025-11-03 23:55:21 +0100

    style: format

--- nordicNRPlus/locationDataFromCOAPToLwm2m.ts
+++ nordicNRPlus/locationDataFromCOAPToLwm2m.ts
@@ -5,16 +5,16 @@ export const locationDataFromCOAPToLwm2m = (
 	location: LocationData,
 	ts: number,
 ): LwM2MObjectInstance[] => [
-		{
-			ObjectID: 14201,
-			ObjectVersion: '1.0',
-			ObjectInstanceID: location.src === 'WIFI' ? 1 : 0,
-			Resources: {
-				'0': location.lat,
-				'1': location.lon,
-				'3': location.uncertainty,
-				'6': location.src,
-				'99': ts / 1000,
-			},
+	{
+		ObjectID: 14201,
+		ObjectVersion: '1.0',
+		ObjectInstanceID: location.src === 'WIFI' ? 1 : 0,
+		Resources: {
+			'0': location.lat,
+			'1': location.lon,
+			'3': location.uncertainty,
+			'6': location.src,
+			'99': ts / 1000,
 		},
-	]
+	},
+]
--- nordicNRPlus/thingExists.ts
+++ nordicNRPlus/thingExists.ts
@@ -1,13 +1,12 @@
 import { type IoTClient, DescribeThingCommand } from '@aws-sdk/client-iot'
 
-export const thingExists = (
-	iotClient: IoTClient) => async (
-	thingName: string,
-): Promise<boolean> => {
-	try {
-		await iotClient.send(new DescribeThingCommand({ thingName }))
-		return true
-	} catch {
-		return false
+export const thingExists =
+	(iotClient: IoTClient) =>
+	async (thingName: string): Promise<boolean> => {
+		try {
+			await iotClient.send(new DescribeThingCommand({ thingName }))
+			return true
+		} catch {
+			return false
+		}
 	}
-}
